8aa1eec6121006c51e10807a9608a567ba72c937
LinGe <35224561+Lin515@users.noreply.github.com>
2022-06-11 21:07:06 +0800

    为每个MOD配一个图片

--- release/vpk.bat
+++ release/vpk.bat
@@ -13,6 +13,7 @@ rd /s /q %all%
 mkdir %all%\scripts\vscripts\LinGe
 mkdir %all%\scripts\vscripts\VSLib
 copy %all%-addoninfo.txt %all%\addoninfo.txt
+copy %all%-addoninfo.jpg %all%\addoninfo.jpg
 copy %main%\LinGe %all%\scripts\vscripts\LinGe
 copy %main%\VSLib %all%\scripts\vscripts\VSLib
 copy %main%\VSLib.nut %all%\scripts\vscripts\VSLib.nut
@@ -25,6 +26,7 @@ rd /s /q %Base%
 mkdir %Base%\scripts\vscripts\LinGe
 mkdir %Base%\scripts\vscripts\VSLib
 copy %Base%-addoninfo.txt %Base%\addoninfo.txt
+copy %Base%-addoninfo.jpg %Base%\addoninfo.jpg
 copy %main%\LinGe\Base.nut %Base%\scripts\vscripts\LinGe\Base.nut
 copy %main%\VSLib %Base%\scripts\vscripts\VSLib
 copy %main%\VSLib.nut %Base%\scripts\vscripts\VSLib.nut
@@ -36,6 +38,7 @@ copy %main%\scriptedmode_addon.nut %Base%\scripts\vscripts\scriptedmode_addon.nu
 rd /s /q %HUD%
 mkdir %HUD%\scripts\vscripts\LinGe
 copy %HUD%-addoninfo.txt %HUD%\addoninfo.txt
+copy %HUD%-addoninfo.jpg %HUD%\addoninfo.jpg
 copy %main%\LinGe\HUD.nut %HUD%\scripts\vscripts\LinGe\HUD.nut
 %vpk% %HUD%
 
@@ -43,6 +46,7 @@ copy %main%\LinGe\HUD.nut %HUD%\scripts\vscripts\LinGe\HUD.nut
 rd /s /q %MoreSI%
 mkdir %MoreSI%\scripts\vscripts\LinGe
 copy %MoreSI%-addoninfo.txt %MoreSI%\addoninfo.txt
+copy %MoreSI%-addoninfo.jpg %MoreSI%\addoninfo.jpg
 copy %main%\LinGe\MoreSI.nut %MoreSI%\scripts\vscripts\LinGe\MoreSI.nut
 %vpk% %MoreSI%
 
@@ -50,6 +54,7 @@ copy %main%\LinGe\MoreSI.nut %MoreSI%\scripts\vscripts\LinGe\MoreSI.nut
 rd /s /q %Hint%
 mkdir %Hint%\scripts\vscripts\LinGe
 copy %Hint%-addoninfo.txt %Hint%\addoninfo.txt
+copy %Hint%-addoninfo.jpg %Hint%\addoninfo.jpg
 copy %main%\LinGe\Hint.nut %Hint%\scripts\vscripts\LinGe\Hint.nut
 %vpk% %Hint%
 
@@ -57,6 +62,7 @@ copy %main%\LinGe\Hint.nut %Hint%\scripts\vscripts\LinGe\Hint.nut
 rd /s /q %zs%
 mkdir %zs%\scripts\vscripts\LinGe
 copy %zs%-addoninfo.txt %zs%\addoninfo.txt
+copy %zs%-addoninfo.jpg %zs%\addoninfo.jpg
 copy %main%\LinGe\zs.nut %zs%\scripts\vscripts\LinGe\zs.nut
 %vpk% %zs%
 
