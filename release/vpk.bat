f0319a3780d225d868b10ea0c4ff5d29c84607c3
LinGe <35224561+Lin515@users.noreply.github.com>
2022-06-13 11:29:45 +0800

    图片文件名弄错了

--- release/vpk.bat
+++ release/vpk.bat
@@ -13,7 +13,7 @@ rd /s /q %all%
 mkdir %all%\scripts\vscripts\LinGe
 mkdir %all%\scripts\vscripts\VSLib
 copy %all%-addoninfo.txt %all%\addoninfo.txt
-copy %all%-addoninfo.jpg %all%\addoninfo.jpg
+copy %all%-addonimage.jpg %all%\addonimage.jpg
 copy %main%\LinGe %all%\scripts\vscripts\LinGe
 copy %main%\VSLib %all%\scripts\vscripts\VSLib
 copy %main%\VSLib.nut %all%\scripts\vscripts\VSLib.nut
@@ -26,7 +26,7 @@ rd /s /q %Base%
 mkdir %Base%\scripts\vscripts\LinGe
 mkdir %Base%\scripts\vscripts\VSLib
 copy %Base%-addoninfo.txt %Base%\addoninfo.txt
-copy %Base%-addoninfo.jpg %Base%\addoninfo.jpg
+copy %Base%-addonimage.jpg %Base%\addonimage.jpg
 copy %main%\LinGe\Base.nut %Base%\scripts\vscripts\LinGe\Base.nut
 copy %main%\VSLib %Base%\scripts\vscripts\VSLib
 copy %main%\VSLib.nut %Base%\scripts\vscripts\VSLib.nut
@@ -38,7 +38,7 @@ copy %main%\scriptedmode_addon.nut %Base%\scripts\vscripts\scriptedmode_addon.nu
 rd /s /q %HUD%
 mkdir %HUD%\scripts\vscripts\LinGe
 copy %HUD%-addoninfo.txt %HUD%\addoninfo.txt
-copy %HUD%-addoninfo.jpg %HUD%\addoninfo.jpg
+copy %HUD%-addonimage.jpg %HUD%\addonimage.jpg
 copy %main%\LinGe\HUD.nut %HUD%\scripts\vscripts\LinGe\HUD.nut
 %vpk% %HUD%
 
@@ -46,7 +46,7 @@ copy %main%\LinGe\HUD.nut %HUD%\scripts\vscripts\LinGe\HUD.nut
 rd /s /q %MoreSI%
 mkdir %MoreSI%\scripts\vscripts\LinGe
 copy %MoreSI%-addoninfo.txt %MoreSI%\addoninfo.txt
-copy %MoreSI%-addoninfo.jpg %MoreSI%\addoninfo.jpg
+copy %MoreSI%-addonimage.jpg %MoreSI%\addonimage.jpg
 copy %main%\LinGe\MoreSI.nut %MoreSI%\scripts\vscripts\LinGe\MoreSI.nut
 %vpk% %MoreSI%
 
@@ -54,7 +54,7 @@ copy %main%\LinGe\MoreSI.nut %MoreSI%\scripts\vscripts\LinGe\MoreSI.nut
 rd /s /q %Hint%
 mkdir %Hint%\scripts\vscripts\LinGe
 copy %Hint%-addoninfo.txt %Hint%\addoninfo.txt
-copy %Hint%-addoninfo.jpg %Hint%\addoninfo.jpg
+copy %Hint%-addonimage.jpg %Hint%\addonimage.jpg
 copy %main%\LinGe\Hint.nut %Hint%\scripts\vscripts\LinGe\Hint.nut
 %vpk% %Hint%
 
@@ -62,7 +62,7 @@ copy %main%\LinGe\Hint.nut %Hint%\scripts\vscripts\LinGe\Hint.nut
 rd /s /q %zs%
 mkdir %zs%\scripts\vscripts\LinGe
 copy %zs%-addoninfo.txt %zs%\addoninfo.txt
-copy %zs%-addoninfo.jpg %zs%\addoninfo.jpg
+copy %zs%-addonimage.jpg %zs%\addonimage.jpg
 copy %main%\LinGe\zs.nut %zs%\scripts\vscripts\LinGe\zs.nut
 %vpk% %zs%
 
